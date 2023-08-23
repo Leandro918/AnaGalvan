3fc28960d3ce353466eaf9e1a622bf2839cedf26
leandro918 <silvaleandro2509@gmail.com>
2023-08-22 23:02:01 -0300

    update01

---
+++ src/js/dataBase.js
@@ -0,0 +1,67 @@
+const base = [
+    {
+        img : "./src/acets/blondes/01.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/02.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/03.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/04.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/05.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/06.jpg",
+        type : "blondes" 
+    },
+    {
+        img : "./src/acets/blondes/07.jpg",
+        type : "blondes" 
+    },
+
+    {
+        img : "./src/acets/hairstyle/01.jpg",
+        type : "hairstyle" 
+    },
+    {
+        img : "./src/acets/hairstyle/02.jpg",
+        type : "hairstyle" 
+    },
+    {
+        img : "./src/acets/hairstyle/03.jpg",
+        type : "hairstyle" 
+    },
+    {
+        img : "./src/acets/hairstyle/04.jpg",
+        type : "hairstyle" 
+    },
+    {
+        img : "./src/acets/hairstyle/05.jpg",
+        type : "hairstyle" 
+    },
+
+    {
+        img : "./src/acets/bride/01.jpg",
+        type : "bride" 
+    },
+    {
+        img : "./src/acets/bride/02.jpg",
+        type : "bride" 
+    },
+    {
+        img : "./src/acets/bride/03.jpg",
+        type : "bride" 
+    }
+   
+
+]
+
---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+let boxModels    = document.querySelector(".box_models")
+let boxHairstyle = document.querySelector(".box_hairstyle")
+let boxBlondes   = document.querySelector(".box_blondes")
+let boxBride     = document.querySelector(".box_bride")
+
+function addCards(box) {
+     box.forEach(element => {
+        if (element.type == "blondes") {
+            boxBlondes.appendChild(createCards(element))
+        }else if(element.type == "hairstyle"){
+            boxHairstyle.appendChild(createCards(element))
+        }else if (element.type == "models") {
+            boxModels.appendChild(createCards(element))
+        }else if (element.type == "bride") {
+            boxBride.appendChild(createBrides(element))
+        }
+     });
+
+}
+addCards(base)
+
+function createCards(element) {
+    let tagImg = document.createElement('img')
+    let tagLi  = document.createElement('li')
+    
+    tagImg.classList.add("photos_models")
+    tagLi.classList.add("box_photos--models")
+
+    tagImg.src = element.img
+
+    tagLi.append(tagImg)
+
+    return tagLi
+
+}
+
+function createBrides(element) {
+    let tagImg = document.createElement('img')
+    let tagLi  = document.createElement('li')
+    
+    tagImg.classList.add("photos_brides")
+    tagLi.classList.add("box_photos--brides")
+
+    tagImg.src = element.img
+
+    tagLi.append(tagImg)
+
+    return tagLi
+
+}
+
+{/* <li class="box_photos--models">
+      <img class="photos_models" src="./src/acets/models/01.jpg" alt="">
+</li> */}
